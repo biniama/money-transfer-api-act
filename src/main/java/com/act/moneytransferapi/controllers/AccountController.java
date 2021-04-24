3f7c6a29f8b4754ab0484d30caf4ee509a8e980d
Biniam Asnake <biniam.asnake@kaufda.de>
2021-04-24 15:34:37 +0200

    List accounts and show account by id are implemented

--- src/main/java/com/act/moneytransferapi/controllers/AccountController.java
+++ src/main/java/com/act/moneytransferapi/controllers/AccountController.java
@@ -3,10 +3,9 @@ package com.act.moneytransferapi.controllers;
 import com.act.moneytransferapi.domains.Account;
 import com.act.moneytransferapi.services.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author Biniam Asnake
@@ -21,5 +20,15 @@ public class AccountController {
     @PostMapping(value = "/create", produces = "application/json")
     public Account createAccount(@RequestBody Account accountRequest) {
         return accountService.createAccount(accountRequest);
+    }
+
+    @GetMapping("/list")
+    public Iterable<Account> getAllAccounts() {
+        return accountService.getAllAccounts();
+    }
+
+    @GetMapping("/show/{id}")
+    public Account getAccountById(@PathVariable("id") String id) {
+        return accountService.getAccountById(id);
     }
 }
--- src/main/java/com/act/moneytransferapi/services/AccountService.java
+++ src/main/java/com/act/moneytransferapi/services/AccountService.java
@@ -5,6 +5,8 @@ import com.act.moneytransferapi.respositories.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 /**
  * @author Biniam Asnake
  */
@@ -14,5 +16,19 @@ public class AccountService {
     private AccountRepository accountRepository;
     public Account createAccount(Account account) {
         return accountRepository.save(account);
+    }
+
+    public Iterable<Account> getAllAccounts() {
+        return accountRepository.findAll();
+    }
+
+    public Account getAccountById(String idString) {
+
+        // converting String to Long
+        Long idLong = Long.valueOf(idString);
+
+        Optional<Account> accountById = accountRepository.findById(idLong);
+
+        return accountById.orElse(null);
     }
 }
