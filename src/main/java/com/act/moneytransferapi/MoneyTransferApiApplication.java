60e5e17a9db139b4e1d13597ad2d25c4141ca244
Biniam Asnake <biniam.asnake@kaufda.de>
2021-04-24 09:35:39 +0200

    Account entity created

---
+++ src/main/java/com/act/moneytransferapi/MoneyTransferApiApplication.java
@@ -0,0 +1,13 @@
+package com.act.moneytransferapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyTransferApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyTransferApiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/act/moneytransferapi/domains/Account.java
@@ -0,0 +1,43 @@
+package com.act.moneytransferapi.domains;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+/**
+ * Account class that defines the properties and will be mapped to a table using ORM.
+ *
+ * @author Biniam Asnake
+ */
+@Data
+@Entity
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    @NotBlank(message = "First name cannot be empty.")
+    private String firstName;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Last name cannot be empty.")
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String phoneNumber;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private Integer pin;
+
+    @Column(nullable = false)
+    private Boolean isVerified = Boolean.TRUE;
+
+    @Column(nullable = false)
+    private Double balance = 0.0;
+}
