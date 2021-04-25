ea3d0637674cd14936330f3786e89c37342beb33
Biniam Asnake <biniam.asnake@kaufda.de>
2021-04-25 11:18:12 +0200

    added CORS config

--- src/main/java/com/act/moneytransferapi/MoneyTransferApiApplication.java
+++ src/main/java/com/act/moneytransferapi/MoneyTransferApiApplication.java
@@ -2,6 +2,9 @@ package com.act.moneytransferapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MoneyTransferApiApplication {
@@ -10,4 +13,14 @@ public class MoneyTransferApiApplication {
         SpringApplication.run(MoneyTransferApiApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*");
+            }
+        };
+    }
 }
